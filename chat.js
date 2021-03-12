1eea3d52784e4fb751ba790754cba82d4fe99e93
Ben Kern <bkern1@lion.lmu.edu>
2021-03-12 10:53:12 -0800

    removed specials.js

--- chat.js
+++ chat.js
@@ -2,7 +2,24 @@ let chats = [];
 
 const anonymous = `<span style="color:darkgray"> (Anonymous) </span>`;
 
-const specials = require('./specials');
+const specials = {
+    ":coffee:": `<img src="https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG" class="chatImage"/>`,
+    "crowdventure": `<a href="http://crowdventure.me/">crowdventure</a>`,
+    ":love:": `<img src="https://www.eharmony.co.uk/dating-advice/wp-content/uploads/2018/04/whatislove-960x640.jpg" class="chatImage"/>`,
+    "josh": `<span style="color: red">chad</span>`,
+    "ben": `<span style="color: red">chad</span>`,
+    "dom": `<span style="color: red">chad</span>`,
+    "evan": `<span style="color: red">chad</span>`,
+    "soph": `<span style="color: red">chad</span>`,
+    "diego": `<span style="color: red">chad</span>`,
+    "marissa": `<span style="color: red">chad</span>`,
+    "herb": `<span style="color: red">chad</span>`,
+    "andrew": `<span style="color: red">chad</span>`,
+    ":dylan:": `<img src="https://resizing.flixster.com/Gc8BERNydw7dUYBPSG0-BFNk5SA=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/166890/166890_v9_bb.jpg" class="chatImage"/>`,
+    "wally": `<span style="color: blue; text-shadow: 5px 5px white;">thad</span>`,
+    "__": `<i>`,
+    "**": `<b>`
+};
 
 const url = 'https://4r52fybt27.execute-api.us-east-1.amazonaws.com/dev/';
 const maxChatLength = 140;
